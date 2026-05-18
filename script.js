6f84c12245776e05c4d780daab9cdfbbd20bd552
caesar-dotcom <aworcaesar@gmail.com>
2026-05-18 08:39:15 -0700

    lastest

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function createPost() {
- 
+/* =========================
+        script.js
+========================= */
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+  slides.forEach((slide)=>{
+    slide.classList.remove("active");
+  });
+
+  slides[index].classList.add("active");
 }
+
+function nextSlide(){
+
+  currentSlide++;
+
+  if(currentSlide >= slides.length){
+    currentSlide = 0;
+  }
+
+  showSlide(currentSlide);
+}
+
+/* Continuous infinite slideshow */
+setInterval(nextSlide, 3000);
